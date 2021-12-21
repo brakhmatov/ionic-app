729947b2d041005c34cbbf0720f6be543e52f580
Bakhtier Rakhmatov <rakhmatov@gmail.com>
2021-12-21 02:51:19 -0500

    Added firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as firebase from "firebase/app";
+import { environment } from 'environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    firebase.initializeApp(environment.firebase);
+  }
 }
